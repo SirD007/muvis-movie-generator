d152e0c77ebc054a59469e8012e4a6e6751eb161
SirD007 <thedavid2009@gmail.com>
2023-08-30 15:14:39 -0300

    HTML e style finished

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'roxo':'#8323FF',
+        'rosa': '#FF2DAF',
+        'amarelo': '#FEEA35',
+      },
+    },
+  },
+  plugins: [],
+}
